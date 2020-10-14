c27cca3d26d94f6800cf39788196d5face335389
Adam Snyder <asnyder@ea.com>
2020-10-13 20:29:05 -0700

    Draw board

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/nsf/termbox-go"
+)
+
+const boardSize = 8
 
 func main() {
-	fmt.Println("Welcome to Othelgo!")
+	if err := termbox.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	drawBoard()
+
+	if err := termbox.Flush(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func drawBoard() {
+	for i := 0; i < boardSize*2+1; i++ {
+		for j := 0; j < boardSize*2+1; j++ {
+			var value rune
+			switch {
+			case j%2 == 0 && i%2 == 0:
+				value = '+'
+			case j%2 == 0:
+				value = '-'
+			case i%2 == 0:
+				value = '|'
+			}
+			termbox.SetCell(i, j, value, termbox.ColorMagenta, termbox.ColorDefault)
+		}
+	}
 }
