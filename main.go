6bec37269a59e06bb5d62ef1946c91bcc63e19c8
neal <nakfoury@gmail.com>
2020-10-13 21:53:35 -0700

    widen board. dial-a-websocket

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"time"
+	"io/ioutil"
 	"log"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/nsf/termbox-go"
 )
 
 const boardSize = 8
 
 func main() {
+	c, res, err := websocket.DefaultDialer.Dial("wss://1y9vcb5geb.execute-api.us-west-2.amazonaws.com/development", nil) // returns conn, HTTPres, err
+	log.Println(res)
+	str, _ := ioutil.ReadAll(res.Body)
+	log.Println(string(str))
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
 	if err := termbox.Init(); err != nil {
 		log.Fatal(err)
 	}
@@ -21,19 +32,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	time.Sleep(5*time.Second)
+	termbox.SetCursor(2, 1)
+	if err := termbox.Flush(); err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(30 * time.Second)
 }
 
 func drawBoard() {
-	for i := 0; i < boardSize*2+1; i++ {
+	for i := 0; i < boardSize*4+1; i++ {
 		for j := 0; j < boardSize*2+1; j++ {
 			var value rune
 			switch {
-			case j%2 == 0 && i%2 == 0:
+			case j%2 == 0 && i%4 == 0:
 				value = '+'
 			case j%2 == 0:
 				value = '-'
-			case i%2 == 0:
+			case i%4 == 0:
 				value = '|'
 			}
 			termbox.SetCell(i, j, value, termbox.ColorMagenta, termbox.ColorDefault)
