4c402b666132327f2f71a6d01ba754778283896e
Adam Snyder <asnyder@ea.com>
2020-10-14 20:07:18 -0700

    Minor deploy_server.sh refactor

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-export AWS_PAGER=""
-source aws-creds.sh
+# Build
 mkdir -p bin
 GOOS=linux go build -o bin/server ./cmd/server
+
+# Zip
 (cd bin && zip server.zip server)
-aws lambda update-function-code --region us-west-2 --function-name othelgoServer --zip-file fileb://bin/server.zip --publish
+
+# Deploy
+AWS_PAGER="" aws lambda update-function-code --region us-west-2 --function-name othelgoServer --zip-file fileb://bin/server.zip --publish
