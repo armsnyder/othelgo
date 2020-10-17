770fd3958893a79a29c2e9a46fe75738794b090e
Adam Snyder <asnyder@ea.com>
2020-10-16 19:36:51 -0700

    Handle errors in deploy_server.sh

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -2,10 +2,10 @@
 
 # Build
 mkdir -p bin
-GOOS=linux go build -o bin/server ./cmd/server
+GOOS=linux go build -o bin/server ./cmd/server || exit 1
 
 # Zip
-(cd bin && zip server.zip server)
+(cd bin && zip server.zip server) || exit 1
 
 # Deploy
 AWS_PAGER="" aws lambda update-function-code --region us-west-2 --function-name othelgoServer --zip-file fileb://bin/server.zip --publish
