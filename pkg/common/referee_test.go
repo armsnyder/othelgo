e28d94f5e9ed424f3e3e44c40a197c442561fcce
Adam Snyder <asnyder@ea.com>
2020-10-24 16:05:30 -0700

    Shorten test

--- pkg/common/referee_test.go
+++ pkg/common/referee_test.go
@@ -6,8 +6,10 @@ import (
 	. "github.com/armsnyder/othelgo/pkg/common"
 )
 
-func buildTestBoard(p1, p2 [][2]int) (board Board) {
-	for i, moves := range [][][2]int{p1, p2} {
+type move [2]int
+
+func buildTestBoard(p1, p2 []move) (board Board) {
+	for i, moves := range [][]move{p1, p2} {
 		player := i + 1
 		for _, move := range moves {
 			x, y := move[0], move[1]
@@ -25,179 +27,155 @@ func TestApplyMove(t *testing.T) {
 		player int
 	}
 	tests := []struct {
-		name        string
-		args        args
-		wantBoard   Board
-		wantUpdated bool
+		name           string
+		args           args
+		wantBoard      Board
+		wantNotUpdated bool
 	}{
 		{
 			name: "flip one short vector player 1",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}},
-					[][2]int{{1, 2}},
+					[]move{{1, 1}},
+					[]move{{1, 2}},
 				),
 				x:      1,
 				y:      3,
 				player: 1,
 			},
 			wantBoard: buildTestBoard(
-				[][2]int{{1, 1}, {1, 2}, {1, 3}},
-				[][2]int{},
+				[]move{{1, 1}, {1, 2}, {1, 3}},
+				nil,
 			),
-			wantUpdated: true,
 		},
 		{
 			name: "flip one short vector player 2",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}},
-					[][2]int{{1, 2}},
+					[]move{{1, 1}},
+					[]move{{1, 2}},
 				),
 				x:      1,
 				y:      0,
 				player: 2,
 			},
 			wantBoard: buildTestBoard(
-				[][2]int{},
-				[][2]int{{1, 0}, {1, 1}, {1, 2}},
+				nil,
+				[]move{{1, 0}, {1, 1}, {1, 2}},
 			),
-			wantUpdated: true,
 		},
 		{
 			name: "flip one long vector",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}, {1, 2}, {1, 3}},
-					[][2]int{{1, 0}},
+					[]move{{1, 1}, {1, 2}, {1, 3}},
+					[]move{{1, 0}},
 				),
 				x:      1,
 				y:      4,
 				player: 2,
 			},
 			wantBoard: buildTestBoard(
-				[][2]int{},
-				[][2]int{{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}},
+				nil,
+				[]move{{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}},
 			),
-			wantUpdated: true,
 		},
 		{
 			name: "flip all vectors",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}, {1, 3}, {1, 5}, {3, 5}, {5, 5}, {5, 3}, {5, 1}, {3, 1}},
-					[][2]int{{2, 2}, {2, 3}, {2, 4}, {3, 4}, {4, 4}, {4, 3}, {4, 2}, {3, 2}},
+					[]move{{1, 1}, {1, 3}, {1, 5}, {3, 5}, {5, 5}, {5, 3}, {5, 1}, {3, 1}},
+					[]move{{2, 2}, {2, 3}, {2, 4}, {3, 4}, {4, 4}, {4, 3}, {4, 2}, {3, 2}},
 				),
 				x:      3,
 				y:      3,
 				player: 1,
 			},
 			wantBoard: buildTestBoard(
-				[][2]int{
+				[]move{
 					{1, 1}, {1, 3}, {1, 5}, {3, 5}, {5, 5}, {5, 3}, {5, 1}, {3, 1},
 					{2, 2}, {2, 3}, {2, 4}, {3, 4}, {4, 4}, {4, 3}, {4, 2}, {3, 2},
 					{3, 3},
 				},
-				[][2]int{},
+				nil,
 			),
-			wantUpdated: true,
 		},
 		{
 			name: "illegal player number",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}},
-					[][2]int{{1, 2}},
+					[]move{{1, 1}},
+					[]move{{1, 2}},
 				),
 				x:      1,
 				y:      3,
 				player: 3,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{{1, 1}},
-				[][2]int{{1, 2}},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 		{
-			name: "illegal position off board",
+			name: "illegal move off board",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{1, 1}},
-					[][2]int{{1, 2}},
+					[]move{{1, 1}},
+					[]move{{1, 2}},
 				),
 				x:      -4,
 				y:      3,
 				player: 1,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{{1, 1}},
-				[][2]int{{1, 2}},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 		{
-			name: "illegal position no adjacent opponent",
+			name: "illegal move no adjacent opponent",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{{3, 3}, {4, 4}},
-					[][2]int{{3, 4}, {4, 3}},
+					[]move{{3, 3}, {4, 4}},
+					[]move{{3, 4}, {4, 3}},
 				),
 				x:      2,
 				y:      2,
 				player: 1,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{{3, 3}, {4, 4}},
-				[][2]int{{3, 4}, {4, 3}},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 		{
-			name: "illegal position no bounding disk",
+			name: "illegal move no bounding disk",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{},
-					[][2]int{{1, 1}, {1, 2}},
+					nil,
+					[]move{{1, 1}, {1, 2}},
 				),
 				x:      1,
 				y:      3,
 				player: 1,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{},
-				[][2]int{{1, 1}, {1, 2}},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 		{
 			name: "illegal position cell already occupied",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{},
-					[][2]int{{1, 1}, {1, 2}},
+					nil,
+					[]move{{1, 1}, {1, 2}},
 				),
 				x:      1,
 				y:      2,
 				player: 1,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{},
-				[][2]int{{1, 1}, {1, 2}},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 		{
 			name: "illegal position board full",
 			args: args{
 				board: buildTestBoard(
-					[][2]int{
+					[]move{
 						{4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, {4, 6}, {4, 7},
 						{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {5, 6}, {5, 7},
 						{6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {6, 6}, {6, 7},
 						{7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7},
 					},
-					[][2]int{
+					[]move{
 						{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
 						{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7},
 						{2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6}, {2, 7},
@@ -208,31 +186,20 @@ func TestApplyMove(t *testing.T) {
 				y:      2,
 				player: 1,
 			},
-			wantBoard: buildTestBoard(
-				[][2]int{
-					{4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, {4, 6}, {4, 7},
-					{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {5, 6}, {5, 7},
-					{6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {6, 6}, {6, 7},
-					{7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7},
-				},
-				[][2]int{
-					{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
-					{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7},
-					{2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6}, {2, 7},
-					{3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {3, 6}, {3, 7},
-				},
-			),
-			wantUpdated: false,
+			wantNotUpdated: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantNotUpdated {
+				tt.wantBoard = tt.args.board
+			}
 			gotBoard, gotUpdated := ApplyMove(tt.args.board, tt.args.x, tt.args.y, tt.args.player)
 			if gotBoard != tt.wantBoard {
 				t.Errorf("ApplyMove() got board = %v, want %v", gotBoard, tt.wantBoard)
 			}
-			if gotUpdated != tt.wantUpdated {
-				t.Errorf("ApplyMove() got updated = %v, want %v", gotUpdated, tt.wantUpdated)
+			if gotUpdated == tt.wantNotUpdated {
+				t.Errorf("ApplyMove() got updated = %v, want %v", gotUpdated, !tt.wantNotUpdated)
 			}
 		})
 	}
