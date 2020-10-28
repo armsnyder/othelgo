6de464c33d90c1fee4175043387916bb53e71870
neal <nakfoury@gmail.com>
2020-10-27 20:23:28 -0700

    Add basic benchmark test for ApplyMove

--- pkg/common/referee_test.go
+++ pkg/common/referee_test.go
@@ -204,3 +204,64 @@ func TestApplyMove(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkApplyMove(b *testing.B) {
+	type args struct {
+		board  Board
+		x      int
+		y      int
+		player Disk
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "benchmark case 1",
+			args: args{
+				board: buildTestBoard(
+					[]move{
+						{3, 3}, {4, 4},
+					},
+					[]move{
+						{3, 4}, {4, 3},
+					},
+				),
+				x:      3,
+				y:      5,
+				player: 1,
+			},
+		},
+		{
+			name: "benchmark case 2",
+			args: args{
+				board: buildTestBoard(
+					[]move{
+						{4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, {4, 6}, {4, 7},
+						{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {5, 6}, {5, 7},
+						{6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {6, 6}, {6, 7},
+						{7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7},
+					},
+					[]move{
+						{0, 0}, {0, 1}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+						{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}, {1, 7},
+						{2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {2, 6}, {2, 7},
+						{3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {3, 6}, {3, 7},
+					},
+				),
+				x:      0,
+				y:      2,
+				player: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			b.ReportAllocs()
+
+			for i := 0; i < b.N; i++ {
+				ApplyMove(tt.args.board, tt.args.x, tt.args.y, tt.args.player)
+			}
+		})
+	}
+}
